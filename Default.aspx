3ee09bfe492b9c19af62b2c1b640df5e50570af0
Chyanne Haugen <chyhaugen@gmail.com>
2019-11-27 09:22:48 -0800

    page for login

---
+++ Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="recipeFinder._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <p>
+        &nbsp;</p>
+    <p>
+        New User:</p>
+    <p>
+        <asp:TextBox ID="newUserName" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:TextBox ID="newPassword" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:TextBox ID="reNewPassword" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="CreateUser" runat="server" Text="Button" OnClick="CreateUser_Click" />
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        Returning User:</p>
+    <p>
+        <asp:TextBox ID="UserName" runat="server"></asp:TextBox>
+   
+    
+   
+    </p>
+    <asp:TextBox ID="Password" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Login" runat="server" OnClick="Login_Click" Text="Button" />
+
+    <br />
+    <br />
+    <asp:TextBox ID="Output" runat="server"></asp:TextBox>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+namespace recipeFinder
+{
+    public partial class _Default : Page
+    {
+        UserHandler userClient = new UserHandler();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            if (userClient.getUser(UserName.Text, Password.Text))
+            {
+                Session["User"] = UserName.Text;
+                Response.Redirect("homepage.aspx", true);
+            }
+            else
+            {
+                Output.Text = "Login failed. Please try again!";
+            }
+        }
+
+        protected void CreateUser_Click(object sender, EventArgs e)
+        {
+            if(userClient.userExist(newUserName.Text))
+            {
+                Output.Text = "User already exist with that name!";
+            }
+            else
+            {
+                if(userClient.createUser(newUserName.Text, newPassword.Text, reNewPassword.Text))
+                {
+                    Output.Text = "User created!";
+                }
+                else
+                {
+                    Output.Text = "Error with creating user. Please try again!";
+                }
+            }
+
+        }
+    }
+}
