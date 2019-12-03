b861cea50c80e5ac434267e17aeec3a5c73298f0
Chyanne Haugen <chyhaugen@gmail.com>
2019-12-03 10:21:02 -0800

    added retry and error logic

---
+++ HolidayAPICall.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Configuration;
+using System.Threading;
+
+namespace recipeFinder
+{
+    public class HolidayAPICall
+    {
+        public string getHoliday(DateTime date)
+        {
+            string year = date.Year.ToString();
+            string day = date.Day.ToString();
+            string month = date.Month.ToString();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://calendarific.com/api/v2/");
+                string holiday_key = ConfigurationManager.AppSettings["HolidayAPI"];
+
+                for(int i = 0; i < 4; i++)
+                {
+                    HttpResponseMessage response = client.GetAsync("holidays?&api_key=" + holiday_key + "&country=US&year=" + year + "&day=" + day + "&month=" + month).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = response.Content.ReadAsStringAsync().Result;
+                        Debug.WriteLine(result);
+                        HolidayObject holiday = JsonConvert.DeserializeObject<HolidayObject>(result);
+                        if (holiday.response.holidays.Length == 0)
+                        {
+                            return "no holiday today";
+                        }
+                        string holidayInfo = holiday.response.holidays[0].name;
+                        return holidayInfo;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Unsuccsessful request. Status code: " + response.StatusCode);
+                        Thread.Sleep(2000 * i);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public class HolidayObject
+    {
+        public Meta meta { get; set; }
+        public Response response { get; set; }
+    }
+
+    public class Meta
+    {
+        public int code { get; set; }
+    }
+
+    public class Response
+    {
+        public Holiday[] holidays { get; set; }
+    }
+
+    public class Holiday
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public Date date { get; set; }
+        public string[] type { get; set; }
+        public string locations { get; set; }
+        public object states { get; set; }
+    }
+
+    public class Date
+    {
+        public string iso { get; set; }
+        public Datetime datetime { get; set; }
+    }
+
+    public class Datetime
+    {
+        public int year { get; set; }
+        public int month { get; set; }
+        public int day { get; set; }
+    }
+
+}
---
+++ SavedRecipes.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace recipeFinder
+{
+    public partial class SavedRecipes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["User"] == null)
+            {
+                Response.Redirect("Default.aspx");
+            }
+            load_recipes();
+        }
+
+        private void load_recipes()
+    {
+            SaveManager sm = new SaveManager();
+            string username = Session["User"].ToString();
+            Dictionary<string, Uri> recipeNameAndURL = sm.getSavedRecipes(username);
+
+            if (recipeNameAndURL == null)
+            {
+                RecipeList.Text = "Sorry. Recipe could not load, please try again later.";
+            }
+            else
+            {
+                foreach (var recipe in recipeNameAndURL)
+                {
+                    RecipeList.Text = RecipeList.Text + recipe.Key + "<br>";
+                }
+            }
+        }
+
+    }
+}
